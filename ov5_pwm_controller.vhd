fb4e7158faa01b2d4735cd0f77895a29c88ee05c
Ellen Johnsen <94108006+Jawny-E@users.noreply.github.com>
2023-10-13 12:53:22 +0200

    Update ov5_pwm_controller.vhd

--- ov5_pwm_controller.vhd
+++ ov5_pwm_controller.vhd
@@ -3,11 +3,14 @@ use ieee.std_logic_1164.all;
 USE ieee.numeric_std.ALL; 
 
 entity exercise5 is
+	--Henta frå oppgåva
+	--Deler på 1000 seinare for å konvertere til ns
 	generic(
-		F_CLK : integer := 50_000_000;
-		T_CLK_PWM : integer := 120;
+		F_CLK : natural := 50;-- MHz
+		T_CLK_PWM : natural := 60; --ns
 		BITS : integer := 3
 	);
+	--Porter prosjektet trenger
 	port(
 		clk, rstn : in std_logic;
 		duty : in std_logic_vector(BITS-1 downto 0);
@@ -18,25 +21,45 @@ end entity exercise5;
 
 
 architecture RTL of exercise5 is
-	constant M : natural := integer(F_CLK*T_CLK_PWM);
-	signal counter : natural range 0 to F_CLK-1;
+
+	constant M : natural := (F_CLK*T_CLK_PWM/1_000); --Forhald mellom klokke-original og klokke-pwm
+	signal clk_pwm : std_logic; --Signal generert etter klokkedivisjon
+	signal counterM : natural range 0 to M; --Counter til klokkedivisjon
+	signal counterPWM : natural range 0 to (2**BITS - 2); --Counter for PWM
+
+	
 	begin
 	
+	--Denne prosessen er klokkedivisjon og gir 
+	--singalet clk_pwm som resultat
 	process(clk, rstn) is
-	
 	begin
 		if rstn = '0' then
-			counter <= 0;
-			pwm_out <= '0';
+			counterM <= 0;
+			clk_pwm <= '0';	
 		elsif rising_edge(clk) then
-			if counter = M-1 then
-				counter <= 0;
-				pwm_out <= '1';
+			if counterM = M-1 then
+				counterM <= 0;
+				clk_pwm <= not clk_pwm;
 			else
-				counter <= counter + 1;
-				if counter < to_integer(unsigned(duty)) then
+				counterM <= counterM + 1;
+			end if;
+		end if;
+	end process;
+	
+	--Denne prosessen handterar pwm-kontroll
+	--gitt signalet clk_pwm
+	process(clk_pwm) is
+	begin
+		if rising_edge(clk_pwm)then
+			if counterPWM = 2**BITS - 2 then
+				counterPWM <= 0;
+				pwm_out <= '0';
+			else
+				counterPWM <= counterPWM + 1;
+				if counterPWM > to_integer(unsigned(duty)) then
 					pwm_out <= '1';
-				else
+				else 
 					pwm_out <= '0';
 				end if;
 			end if;
